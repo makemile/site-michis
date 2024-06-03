7ea9950938e04cd0b85ffee75b375cd024c3a844
Kengya <mkengya@gmail.com>
2024-06-03 18:13:46 -0500

    add styles , function favourites and delete

--- src/Apis/Apis.js
+++ src/Apis/Apis.js
@@ -1,2 +1,4 @@
 export const URL = "https://api.thecatapi.com/v1/images/search?limit=2";
-export const URLFavourite = "https://api.thecatapi.com/v1/favourites?api_key=live_rSg4pIw2MTpkzS1jQ7k7D7a4DshHbRT9xkYFDNE08CWPGajmgocwIamxUKjpPqlf";
+export const URLFavourite = "https://api.thecatapi.com/v1/favourites?api_key=live_VT4dIgojFfBePQVvPb0arFlH3I0C1WUzRkP7IwKzONMGChY19ZSsauPAmA3aPYuf";
+export const API_KEY = 'live_VT4dIgojFfBePQVvPb0arFlH3I0C1WUzRkP7IwKzONMGChY19ZSsauPAmA3aPYuf';
+export const URLFavourite_Delete = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api-key=live_VT4dIgojFfBePQVvPb0arFlH3I0C1WUzRkP7IwKzONMGChY19ZSsauPAmA3aPYuf`;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import { URL } from "./Apis/Apis";
+import { API_KEY, URL, URLFavourite_Delete } from "./Apis/Apis";
 import { URLFavourite } from "./Apis/Apis";
-const container = document.querySelector("#containerCat");
 const sectionCard = document.querySelector(".cardCats");
 const sectionCardFavourite = document.querySelector(".cardFavourites");
-const btnReload = document.querySelector(".btnReload");
+const btnReload = document.querySelector("#btnReload");
 
 const getData = async () => {
   try {
     let render = [];
+    sectionCard.innerHTML = "";
     let response = await fetch(URL);
     let cats = await response.json();
-    cats.forEach((cats, index) => {
+    cats.forEach((cats) => {
       let article = document.createElement("article");
       article.classList.add("item_card");
       let image = document.createElement("img");
@@ -20,7 +20,6 @@ const getData = async () => {
       span.setAttribute("data-tooltip", "Agregar favoritos ♥");
       btnFavourite.classList.add("btnFavourite");
       footer.className = "footer";
-      console.log(cats);
       image.src = cats.url;
       image.alt = cats.name;
       btnFavourite.id = cats.id;
@@ -29,12 +28,11 @@ const getData = async () => {
       span.append(btnFavourite);
       footer.append(span);
       article.append(image, footer);
-      sectionCard.addEventListener("click", (e) => { 
-    if(e.target.id === cats.id){
-        console.log(cats.id,'kkk');
-        saveFavourite(cats.id);
-    }
-      })
+      sectionCard.addEventListener("click", (e) => {
+        if (e.target.id === cats.id) {
+          saveFavourite(cats.id);
+        }
+      });
       render.push(article);
     });
     sectionCard.append(...render);
@@ -43,57 +41,78 @@ const getData = async () => {
   }
 };
 
-async function reloadFavourites(){
-try{
+btnReload.addEventListener("click", () => getData());
+
+async function reloadFavourites() {
+  try {
     let render = [];
     let response = await fetch(URLFavourite);
     let catsFavourites = await response.json();
     catsFavourites.forEach((cats, index) => {
-        let article = document.createElement("article");
-        article.classList.add("item_card");
-        let image = document.createElement("img");
-        let btnFavourite = document.createElement("button");
-        let footer = document.createElement("footer");
-        let span = document.createElement("span");
-        span.setAttribute("data-tooltip", "Sacar favoritos ♥");
-        btnFavourite.classList.add("btnFavourite");
-        footer.className = "footer";
-        console.log(cats);
-        image.src = cats.image.url;
-        image.alt = cats.name;
-        btnFavourite.id = cats.id;
-        btnFavourite.type = "button";
-        btnFavourite.textContent = "-";
-        span.append(btnFavourite);
-        footer.append(span);
-        article.append(image, footer);
-        render.push(article);
+      let article = document.createElement("article");
+      article.classList.add("item_card");
+      let image = document.createElement("img");
+      let btnFavourite = document.createElement("button");
+      let footer = document.createElement("footer");
+      let span = document.createElement("span");
+      span.setAttribute("data-tooltip", "Sacar favoritos ♥");
+      btnFavourite.classList.add("btnFavourite");
+      footer.className = "footer";
+      image.src = cats.image.url;
+      image.alt = cats.name;
+      btnFavourite.id = cats.image_id;
+      btnFavourite.type = "button";
+      btnFavourite.textContent = "-";
+      sectionCardFavourite.addEventListener("click", (e) => {
+        if (e.target.id === cats.image_id) {
+          deleteFavourite(cats.image_id);
+        }
       });
-      sectionCardFavourite.append(...render)
-}catch(error){
+      span.append(btnFavourite);
+      footer.append(span);
+      article.append(image, footer);
+      render.push(article);
+    });
+    sectionCardFavourite.append(...render);
+  } catch (error) {
     console.error(error);
+  }
 }
+
+async function saveFavourite(id) {
+  try {
+    const res = await fetch(URLFavourite, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        image_id: id,
+      }),
+    });
+    reloadFavourites();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-
-async function saveFavourite(id){
-
-    try{
-        const res = await fetch(URLFavourite,{
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                image_id: id
-            })
-        });
-        console.log(res,'res')
-    }catch(error){
-        console.error(error);
+async function deleteFavourite(id) {
+  try {
+    const res = await fetch(URLFavourite_Delete(id), {
+      method: "DELETE",
+      headers: { "x-api-key": API_KEY },
+    });
+    if (res.status !== 200) {
+      const error = await res.text();
+    } else {
+      const data = await res.json();
+      reloadFavourites();
     }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getData();
-saveFavourite();
 reloadFavourites();
